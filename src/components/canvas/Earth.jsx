cd53fe2f177a3d6fa91a5f1ac9814675c764152d
Remy <remy.noulin@epitech.eu>
2024-08-06 14:06:28 +0200

    [MOD] removed pan on 3d models

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -39,6 +39,7 @@ const EarthCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          enablePan={false}
         />
         <Earth />
       </Suspense>
